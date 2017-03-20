241230296c60f3c67284d02ff7772835e4bcc3db
Sebastien Bortolussi <sebastien.bortolussi@orange.com>
2017-03-20 16:19:57 +0100

    Fail fast on bad cloudfoundry credentials
    These changes add support to fail app startup on bad CloudFoundry CC API credentials.
    
    [#41]

---
+++ service-broker-filter-securitygroups/src/main/java/com/orange/cloud/servicebroker/filter/securitygroups/config/CheckCloudFoundryConnection.java
@@ -0,0 +1,27 @@
+package com.orange.cloud.servicebroker.filter.securitygroups.config;
+
+import org.cloudfoundry.client.CloudFoundryClient;
+import org.cloudfoundry.client.v2.info.GetInfoRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.time.Duration;
+
+/**
+ * This code forces a connection to CloudFoundry with provided credentials on app startup.
+ * When bad credentials exception, app fails to start.
+ *
+ */
+@Component
+public class CheckCloudFoundryConnection implements CommandLineRunner {
+
+    @Autowired
+    CloudFoundryClient cloudFoundryClient;
+
+    @Override
+    public void run(String... strings) throws Exception {
+        //basic get on info endpoint so as to perform a CloudFoundry CC API connection and thus assert credentials are valid
+        cloudFoundryClient.info().get(GetInfoRequest.builder().build()).block(Duration.ofMinutes(3));
+    }
+}
