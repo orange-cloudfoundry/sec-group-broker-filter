f4a6a9a45b24a91d39af19d1663350a3b8c6bef5
Guillaume Berche <guillaume.berche@orange.com>
2020-08-28 00:02:05 +0200

    Adapt test to spring boot test current syntax
    Also remove
    
         <dependency>
                <groupId>org.springframework</groupId>
                <artifactId>spring-context</artifactId>
                <version>4.2.8.RELEASE</version>
            </dependency>
    
    which fails test with
    
    ```
    java.lang.AbstractMethodError: Receiver class org.springframework.context.annotation.AnnotationConfigApplicationContext does not define or inherit an implementation of the resolved method 'abstract org.springframework.beans.factory.ObjectProvider getBeanProvider(java.lang.Class)' of interface org.springframework.beans.factory.BeanFactory
    ```

--- service-broker-filter-securitygroups/src/test/java/com/orange/cloud/servicebroker/filter/securitygroups/config/SpecificationConfigTest.java
+++ service-broker-filter-securitygroups/src/test/java/com/orange/cloud/servicebroker/filter/securitygroups/config/SpecificationConfigTest.java
@@ -1,136 +1,176 @@
 package com.orange.cloud.servicebroker.filter.securitygroups.config;
 
-import com.orange.cloud.servicebroker.filter.securitygroups.domain.*;
-import org.assertj.core.api.Assertions;
-import org.junit.After;
+import com.orange.cloud.servicebroker.filter.securitygroups.domain.ImmutableCIDR;
+import com.orange.cloud.servicebroker.filter.securitygroups.domain.ImmutableIPAddress;
+import com.orange.cloud.servicebroker.filter.securitygroups.domain.ImmutableIPRange;
+import com.orange.cloud.servicebroker.filter.securitygroups.domain.ImmutablePort;
+import com.orange.cloud.servicebroker.filter.securitygroups.domain.ImmutablePortRange;
+import com.orange.cloud.servicebroker.filter.securitygroups.domain.ImmutablePorts;
+import com.orange.cloud.servicebroker.filter.securitygroups.domain.ImmutableTrustedDestination;
+import com.orange.cloud.servicebroker.filter.securitygroups.domain.TrustedDestination;
 import org.junit.Test;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.mock.env.MockEnvironment;
+
+import org.springframework.boot.autoconfigure.AutoConfigurations;
+import org.springframework.boot.autoconfigure.logging.ConditionEvaluationReportLoggingListener;
+import org.springframework.boot.logging.LogLevel;
+import org.springframework.boot.test.context.runner.ApplicationContextRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * @author Sebastien Bortolussi
  */
 public class SpecificationConfigTest {
 
-    private AnnotationConfigApplicationContext context;
-
-    @After
-    public void tearDown() {
-        if (this.context != null) {
-            this.context.close();
-            if (this.context.getParent() != null) {
-                ((ConfigurableApplicationContext) this.context.getParent()).close();
-            }
-        }
-    }
+//    private AnnotationConfigApplicationContext context;
+//See https://docs.spring.io/spring-boot/docs/2.1.12.RELEASE/reference/html/boot-features-developing-auto-configuration.html#boot-features-test-autoconfig
+ConditionEvaluationReportLoggingListener conditionEvaluationReportLoggingListener = new ConditionEvaluationReportLoggingListener(
+    LogLevel.INFO);
 
     @Test
     public void default_trusted_destination_config() {
-        MockEnvironment env = new MockEnvironment();
-        this.context = new AnnotationConfigApplicationContext();
-        this.context.setEnvironment(env);
-        this.context.register(SpecificationConfig.DefaultSpecificationConfig.class, SpecificationConfig.AllowedDestinationSpecificationConfig.class, TrustedDestinationConfig.class);
-        this.context.refresh();
-        final TrustedDestination trustedDestination = this.context.getBean(TrustedDestination.class);
-
-        Assertions.assertThat(trustedDestination).isEqualTo(ImmutableTrustedDestination.builder().hosts(ImmutableCIDR.of("0.0.0.0/0")).build());
+        //noinspection unused
+        ApplicationContextRunner contextRunner = new ApplicationContextRunner()
+            .withConfiguration(AutoConfigurations.of(SpecificationConfig.DefaultSpecificationConfig.class, SpecificationConfig.AllowedDestinationSpecificationConfig.class, TrustedDestinationConfig.class))
+            .run((context) -> {
+                assertThat(context)
+                    .getBean(TrustedDestination.class)
+                    .isEqualTo(ImmutableTrustedDestination.builder().hosts(ImmutableCIDR.of("0.0.0.0/0")).build());
+            });
     }
 
     @Test
     public void trusted_destination_config_with_cidr() {
-        MockEnvironment env = new MockEnvironment();
-        this.context = new AnnotationConfigApplicationContext();
-        env.setProperty("broker.filter.trusted.destination.hosts", "192.168.0.1/29");
-        this.context.setEnvironment(env);
-        this.context.register(SpecificationConfig.DefaultSpecificationConfig.class, SpecificationConfig.AllowedDestinationSpecificationConfig.class);
-        this.context.refresh();
-        final TrustedDestination trustedDestination = this.context.getBean(TrustedDestination.class);
-
-        Assertions.assertThat(trustedDestination).isEqualTo(ImmutableTrustedDestination.builder()
-                .hosts(ImmutableCIDR.of("192.168.0.1/29"))
-                .build());
+        //noinspection unused
+        ApplicationContextRunner contextRunner = new ApplicationContextRunner()
+            .withInitializer(conditionEvaluationReportLoggingListener)
+            .withPropertyValues("broker.filter.trusted.destination.hosts=192.168.0.1/29")
+            .withConfiguration(AutoConfigurations.of(SpecificationConfig.DefaultSpecificationConfig.class, SpecificationConfig.AllowedDestinationSpecificationConfig.class))
+            .run((context) -> {
+                assertThat(context)
+                    .getBean(TrustedDestination.class)
+                    .isEqualTo(ImmutableTrustedDestination.builder()
+                        .hosts(ImmutableCIDR.of("192.168.0.1/29"))
+                        .build());
+            });
     }
 
     @Test
     public void trusted_destination_config_with_single_ip() {
-        MockEnvironment env = new MockEnvironment();
-        this.context = new AnnotationConfigApplicationContext();
-        env.setProperty("broker.filter.trusted.destination.hosts", "192.168.0.1");
-        this.context.setEnvironment(env);
-        this.context.register(SpecificationConfig.DefaultSpecificationConfig.class, SpecificationConfig.AllowedDestinationSpecificationConfig.class);
-        this.context.refresh();
-        final TrustedDestination trustedDestination = this.context.getBean(TrustedDestination.class);
-
-        Assertions.assertThat(trustedDestination).isEqualTo(ImmutableTrustedDestination.builder()
-                .hosts(ImmutableIPAddress.of("192.168.0.1"))
-                .build());
+        //noinspection unused
+        ApplicationContextRunner contextRunner = new ApplicationContextRunner()
+            .withInitializer(conditionEvaluationReportLoggingListener)
+            .withPropertyValues("broker.filter.trusted.destination.hosts=192.168.0.1")
+            .withConfiguration(AutoConfigurations.of(SpecificationConfig.DefaultSpecificationConfig.class, SpecificationConfig.AllowedDestinationSpecificationConfig.class))
+            .run((context) -> {
+                assertThat(context)
+                    .getBean(TrustedDestination.class)
+                    .isEqualTo(ImmutableTrustedDestination.builder()
+                        .hosts(ImmutableIPAddress.of("192.168.0.1"))
+                        .build());
+            });
     }
 
     @Test
     public void trusted_destination_config_with_ip_range() {
-        MockEnvironment env = new MockEnvironment();
-        this.context = new AnnotationConfigApplicationContext();
-        env.setProperty("broker.filter.trusted.destination.hosts", "192.168.0.1-192.168.0.9");
-        this.context.setEnvironment(env);
-        this.context.register(SpecificationConfig.DefaultSpecificationConfig.class, SpecificationConfig.AllowedDestinationSpecificationConfig.class);
-        this.context.refresh();
-        final TrustedDestination trustedDestination = this.context.getBean(TrustedDestination.class);
-
-        Assertions.assertThat(trustedDestination).isEqualTo(ImmutableTrustedDestination.builder()
-                .hosts(ImmutableIPRange.builder().from(ImmutableIPAddress.of("192.168.0.1")).to(ImmutableIPAddress.of("192.168.0.9")).build())
-                .build());
+        //noinspection unused
+        ApplicationContextRunner contextRunner = new ApplicationContextRunner()
+            .withInitializer(conditionEvaluationReportLoggingListener)
+            .withPropertyValues("broker.filter.trusted.destination.hosts=192.168.0.1-192.168.0.9")
+            .withConfiguration(AutoConfigurations.of(SpecificationConfig.DefaultSpecificationConfig.class, SpecificationConfig.AllowedDestinationSpecificationConfig.class))
+            .run((context) -> {
+                assertThat(context)
+                    .getBean(TrustedDestination.class)
+                    .isEqualTo(ImmutableTrustedDestination.builder()
+                        .hosts(ImmutableIPRange.builder().from(ImmutableIPAddress.of("192.168.0.1")).to(ImmutableIPAddress.of("192.168.0.9")).build())
+                        .build());
+            });
     }
 
     @Test
     public void trusted_destination_config_with_cidr_and_port_range() {
-        MockEnvironment env = new MockEnvironment();
-        this.context = new AnnotationConfigApplicationContext();
-        env.setProperty("broker.filter.trusted.destination.hosts", "192.168.0.1/29");
-        env.setProperty("broker.filter.trusted.destination.ports", "3306-3310");
-        this.context.setEnvironment(env);
-        this.context.register(SpecificationConfig.DefaultSpecificationConfig.class, SpecificationConfig.AllowedDestinationSpecificationConfig.class);
-        this.context.refresh();
-        final TrustedDestination trustedDestination = this.context.getBean(TrustedDestination.class);
-
-        Assertions.assertThat(trustedDestination).isEqualTo(ImmutableTrustedDestination.builder()
-                .hosts(ImmutableCIDR.of("192.168.0.1/29"))
-                .ports(ImmutablePortRange.builder().from(ImmutablePort.of(3306)).to(ImmutablePort.of(3310)).build())
-                .build());
+        //noinspection unused
+        ApplicationContextRunner contextRunner = new ApplicationContextRunner()
+            .withInitializer(conditionEvaluationReportLoggingListener)
+            .withPropertyValues(
+                "broker.filter.trusted.destination.hosts=192.168.0.1/29",
+                "broker.filter.trusted.destination.ports=3306-3310")
+            .withConfiguration(AutoConfigurations.of(SpecificationConfig.DefaultSpecificationConfig.class, SpecificationConfig.AllowedDestinationSpecificationConfig.class))
+            .run((context) -> {
+                assertThat(context)
+                    .getBean(TrustedDestination.class)
+                    .isEqualTo(ImmutableTrustedDestination.builder()
+                        .hosts(ImmutableCIDR.of("192.168.0.1/29"))
+                        .ports(ImmutablePortRange.builder().from(ImmutablePort.of(3306)).to(ImmutablePort.of(3310)).build())
+                        .build());
+            });
     }
 
     @Test
     public void trusted_destination_config_with_cidr_and_port_list() {
-        MockEnvironment env = new MockEnvironment();
-        this.context = new AnnotationConfigApplicationContext();
-        env.setProperty("broker.filter.trusted.destination.hosts", "192.168.0.1/29");
-        env.setProperty("broker.filter.trusted.destination.ports", "3306,3310");
-        this.context.setEnvironment(env);
-        this.context.register(SpecificationConfig.DefaultSpecificationConfig.class, SpecificationConfig.AllowedDestinationSpecificationConfig.class);
-        this.context.refresh();
-        final TrustedDestination trustedDestination = this.context.getBean(TrustedDestination.class);
+        //noinspection unused
+        ApplicationContextRunner contextRunner = new ApplicationContextRunner()
+            .withInitializer(conditionEvaluationReportLoggingListener)
+            .withPropertyValues(
+                "broker.filter.trusted.destination.hosts=192.168.0.1/29",
+                "broker.filter.trusted.destination.ports=3306,3310")
+            .withConfiguration(AutoConfigurations.of(SpecificationConfig.DefaultSpecificationConfig.class, SpecificationConfig.AllowedDestinationSpecificationConfig.class))
+            .run((context) -> {
+                assertThat(context)
+                    .getBean(TrustedDestination.class)
+                    .isEqualTo(ImmutableTrustedDestination.builder()
+                        .hosts(ImmutableCIDR.of("192.168.0.1/29"))
+                            .ports(ImmutablePorts.builder().addValue(3306).addValue(3310).build())
+                            .build());
+            });
 
-        Assertions.assertThat(trustedDestination).isEqualTo(ImmutableTrustedDestination.builder()
-                .hosts(ImmutableCIDR.of("192.168.0.1/29"))
-                .ports(ImmutablePorts.builder().addValue(3306).addValue(3310).build())
-                .build());
+
+//        MockEnvironment env = new MockEnvironment();
+//        this.context = new AnnotationConfigApplicationContext();
+//        env.setProperty("broker.filter.trusted.destination.hosts", "192.168.0.1/29");
+//        env.setProperty("broker.filter.trusted.destination.ports", "3306,3310");
+//        this.context.setEnvironment(env);
+//        this.context.register(SpecificationConfig.DefaultSpecificationConfig.class, SpecificationConfig.AllowedDestinationSpecificationConfig.class);
+//        this.context.refresh();
+//        final TrustedDestination trustedDestination = this.context.getBean(TrustedDestination.class);
+//
+//        Assertions.assertThat(trustedDestination).isEqualTo(ImmutableTrustedDestination.builder()
+//                .hosts(ImmutableCIDR.of("192.168.0.1/29"))
+//                .ports(ImmutablePorts.builder().addValue(3306).addValue(3310).build())
+//                .build());
     }
 
     @Test
     public void trusted_destination_config_with_cidr_and_single_port() {
-        MockEnvironment env = new MockEnvironment();
-        this.context = new AnnotationConfigApplicationContext();
-        env.setProperty("broker.filter.trusted.destination.hosts", "192.168.0.1/29");
-        env.setProperty("broker.filter.trusted.destination.ports", "3306");
-        this.context.setEnvironment(env);
-        this.context.register(SpecificationConfig.DefaultSpecificationConfig.class, SpecificationConfig.AllowedDestinationSpecificationConfig.class);
-        this.context.refresh();
-        final TrustedDestination trustedDestination = this.context.getBean(TrustedDestination.class);
+        //noinspection unused
+        ApplicationContextRunner contextRunner = new ApplicationContextRunner()
+            .withInitializer(conditionEvaluationReportLoggingListener)
+            .withPropertyValues(
+                "broker.filter.trusted.destination.hosts=192.168.0.1/29",
+                "broker.filter.trusted.destination.ports=3306")
+            .withConfiguration(AutoConfigurations.of(SpecificationConfig.DefaultSpecificationConfig.class, SpecificationConfig.AllowedDestinationSpecificationConfig.class))
+            .run((context) -> {
+                assertThat(context)
+                    .getBean(TrustedDestination.class)
+                    .isEqualTo(ImmutableTrustedDestination.builder()
+                        .hosts(ImmutableCIDR.of("192.168.0.1/29"))
+                        .ports(ImmutablePorts.builder().addValue(3306).build())
+                        .build());
+            });
 
-        Assertions.assertThat(trustedDestination).isEqualTo(ImmutableTrustedDestination.builder()
-                .hosts(ImmutableCIDR.of("192.168.0.1/29"))
-                .ports(ImmutablePorts.builder().addValue(3306).build())
-                .build());
+//        MockEnvironment env = new MockEnvironment();
+//        this.context = new AnnotationConfigApplicationContext();
+//        env.setProperty("broker.filter.trusted.destination.hosts", "192.168.0.1/29");
+//        env.setProperty("broker.filter.trusted.destination.ports", "3306");
+//        this.context.setEnvironment(env);
+//        this.context.register(SpecificationConfig.DefaultSpecificationConfig.class, SpecificationConfig.AllowedDestinationSpecificationConfig.class);
+//        this.context.refresh();
+//        final TrustedDestination trustedDestination = this.context.getBean(TrustedDestination.class);
+//
+//        Assertions.assertThat(trustedDestination).isEqualTo(ImmutableTrustedDestination.builder()
+//                .hosts(ImmutableCIDR.of("192.168.0.1/29"))
+//                .ports(ImmutablePorts.builder().addValue(3306).build())
+//                .build());
     }
 
 }
